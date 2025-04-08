60d4cec363762ecdae63bd9a419390f32b722200
GabrielFs33 <cleitonmiyamoto77@gmail.com>
2025-04-08 09:37:34 -0300

    Redirecionamento entre telas, adicionado TelaJogo

---
+++ telaAbertura.py
@@ -0,0 +1,38 @@
+import tkinter as tk
+from telaInstrucoes import TelaInstrucoes
+from utilitario import resetTela
+
+class TelaInicial:
+    def __init__(self, root):
+        self.root = root
+        resetTela(self.root)
+        self.root.title("THE MATH GAME")
+
+    def frameTelaInicial(self):
+        titulo = tk.Label(self.root, text="Bem-vindo ao \nTHE MATH GAME!", font=("Arial", 14))
+        titulo.pack(pady=50)
+
+       # self.img_play = tk.PhotoImage(file=r'C:\Users\manutencao\Desktop\theMathGame-teste\imagens\play.png')
+
+        btPlay = tk.Button(
+            self.root,
+            #image=self.img_play,
+            text="Play",
+            font=("Arial", 20),
+            width=10,
+            height=2,
+            command=self.irParaInstrucoes
+        )
+        btPlay.pack(pady=20)
+
+
+        rodape = tk.Label(
+            self.root,
+            text="Desenvolvido por GabrielFirmiano e Hugo Miguel (Senai Betim 2025)",
+            font=("Arial", 8)
+        )
+        rodape.pack(side="bottom", pady=10)
+
+    def irParaInstrucoes(self):
+            TelaInstrucoes(self.root).frameTelaInstrucoes()
+
---
+++ telaJogo.py
@@ -0,0 +1,42 @@
+import tkinter as tk 
+from utilitario import resetTela
+class TelaJogo:
+    def __init__(self, root):
+        self.root = root
+
+    def gameFrame(self,root,partida,pontuacao):
+        resetTela(self.root)
+
+        self.root.title("The Math GAME")
+
+        cabecalho = tk.Frame(self.root)
+        cabecalho.pack(pady=10)
+
+        tk.Label(cabecalho, text="Pontuação:").grid(row=0,column=0,padx=10)
+        tk.Label(cabecalho, text="0").grid(row=0,column=1, padx=10),
+
+        tk.Label(cabecalho, text="Partida:").grid(row=0,column=2,padx=10)
+        tk.Label(cabecalho, text="0").grid(row=0,column=3,padx=10)
+
+        tk.Label(cabecalho, text="Tempo:").grid(row=0,column=4,padx=10)
+        tk.Label(cabecalho, text="0").grid(row=0,column=5,padx=10)
+
+        botaoParar = tk.Button(cabecalho, text="Parar", font=("Arial",10), command=lambda: self.pararJogo(root))
+        botaoParar.grid(row=0,column=6,padx=10)
+
+        numerosFrame= tk.Frame(self.root)
+        numerosFrame.pack(pady=40)
+
+        tk.Label(numerosFrame, text="1", font=("Arial", 32)).pack(side="left", padx=20)
+        tk.Label(numerosFrame, text="?", font=("Arial", 32)).pack(side="left", padx=20)
+
+        tk.Label(numerosFrame, text="2", font=("Arial",32)).pack(side="left",padx=20)
+        tk.Label(numerosFrame, text="=", font=("Arial", 32)).pack(side="left",padx=20)
+
+        tk.Label(numerosFrame, text="3",font=("Arial",32)).pack(side="left",padx=20)
+
+        operacoesFrame= tk.Frame(self.root)
+        operacoesFrame.pack(pady=30)
+
+        for operacao in ["+","-","/","*"]:
+            tk.Button(operacoesFrame, text=operacao, font=("Arial",16), width=5, height=2).pack(side="left",padx=10)
